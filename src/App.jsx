d1b936add89567bae2b9ab654c7eb8163e78ca8e
SanyaTitov <alexsandrvladimirovih2008@gmail.com>
2025-11-21 17:53:35 +0500

    Разработка ПО

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './counterSlice'
-
-export function Counter() {
+import { Counter } from './Counter'
+export function App() {
   const count = useSelector(state => state.counter.value)
   const dispatch = useDispatch()
 
   return (
     <div>
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
+     <Counter/>
     </div>
   )
 }
